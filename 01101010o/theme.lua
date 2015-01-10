bbd88167474125befbb2d729686014ab8f1411ee
01101010o <01101010o@openmailbox.org>
2015-01-10 17:17:12 -0300

    themes

---
+++ 01101010o/theme.lua
@@ -0,0 +1,78 @@
+-------------------------------
+-- "01101010o" awesome theme --
+-------------------------------
+
+-- BASICS
+theme = {}
+theme.font          = "sans 8"
+
+theme.bg_normal     = "#222222"
+theme.bg_focus      = "#222222"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#222222"
+theme.bg_systray    = theme.bg_normal
+
+theme.fg_normal     = "#ffffff"
+theme.fg_focus      = "#adff2f"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#000000"
+
+theme.border_width  = 1
+theme.border_normal = "#000000"
+theme.border_focus  = "#000000"
+theme.border_marked = "#91231c"
+
+-- IMAGES
+theme.layout_fairh           = "/usr/share/awesome/themes/01101010o/layouts/fairhw.png"
+theme.layout_fairv           = "/usr/share/awesome/themes/01101010o/layouts/fairvw.png"
+theme.layout_floating        = "/usr/share/awesome/themes/01101010o/layouts/floatingw.png"
+theme.layout_magnifier       = "/usr/share/awesome/themes/01101010o/layouts/magnifierw.png"
+theme.layout_max             = "/usr/share/awesome/themes/01101010o/layouts/maxw.png"
+theme.layout_fullscreen      = "/usr/share/awesome/themes/01101010o/layouts/fullscreenw.png"
+theme.layout_tilebottom      = "/usr/share/awesome/themes/01101010o/layouts/tilebottomw.png"
+theme.layout_tileleft        = "/usr/share/awesome/themes/01101010o/layouts/tileleftw.png"
+theme.layout_tile            = "/usr/share/awesome/themes/01101010o/layouts/tilew.png"
+theme.layout_tiletop         = "/usr/share/awesome/themes/01101010o/layouts/tiletopw.png"
+theme.layout_spiral          = "/usr/share/awesome/themes/01101010o/layouts/spiralw.png"
+theme.layout_dwindle         = "/usr/share/awesome/themes/01101010o/layouts/dwindlew.png"
+
+theme.awesome_icon           = "/usr/share/awesome/themes/01101010o/logo5.png"
+
+-- from default for now...
+theme.menu_submenu_icon     = "/usr/share/awesome/themes/01101010o/submenu.png"
+theme.taglist_squares_sel   = "/usr/share/awesome/themes/01101010o/taglist/squarefw.png"
+theme.taglist_squares_unsel = "/usr/share/awesome/themes/01101010o/taglist/squarew.png"
+
+-- MISC
+theme.wallpaper             = "/usr/share/awesome/themes/01101010o/manjaro-f3.png"
+theme.taglist_squares       = "true"
+theme.titlebar_close_button = "true"
+theme.menu_height           = 15
+theme.menu_width            = 100
+
+-- Define the image to load
+theme.titlebar_close_button_normal = "/usr/share/awesome/themes/01101010o/titlebar/close_normal.png"
+theme.titlebar_close_button_focus = "/usr/share/awesome/themes/01101010o/titlebar/close_focus.png"
+
+theme.titlebar_ontop_button_normal_inactive = "/usr/share/awesome/themes/01101010o/titlebar/ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive = "/usr/share/awesome/themes/01101010o/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = "/usr/share/awesome/themes/01101010o/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active = "/usr/share/awesome/themes/01101010o/titlebar/ontop_focus_active.png"
+
+theme.titlebar_sticky_button_normal_inactive = "/usr/share/awesome/themes/01101010o/titlebar/sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive = "/usr/share/awesome/themes/01101010o/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = "/usr/share/awesome/themes/01101010o/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active = "/usr/share/awesome/themes/01101010o/titlebar/sticky_focus_active.png"
+
+theme.titlebar_floating_button_normal_inactive = "/usr/share/awesome/themes/01101010o/titlebar/floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive = "/usr/share/awesome/themes/01101010o/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = "/usr/share/awesome/themes/01101010o/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_active = "/usr/share/awesome/themes/01101010o/titlebar/floating_focus_active.png"
+
+theme.titlebar_maximized_button_normal_inactive = "/usr/share/awesome/themes/01101010o/titlebar/maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive = "/usr/share/awesome/themes/01101010o/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/01101010o/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active = "/usr/share/awesome/themes/01101010o/titlebar/maximized_focus_active.png"
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4:textwidth=80
